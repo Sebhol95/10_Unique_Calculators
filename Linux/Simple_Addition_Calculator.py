49941a613c545936ad30b061af2bd74b50a49700
Sebastian Holterhuset <39740798+Sebhol95@users.noreply.github.com>
2018-10-02 11:29:19 +0200

    Add files via upload

---
+++ Linux/Simple_Addition_Calculator.py
@@ -0,0 +1,13 @@
+# Created by Sebhol95 for repository "Calculators" https://github.com/Sebhol95/Calculators
+
+x = 1
+print ("\n")
+print ("The Simple addition Calculator")
+print ("")
+x = int(input("Press 1 to calculate or 0 to exit "))
+while (x > 0):
+    num1 = int(input ("Enter your first number: "))
+    num2 = int(input ("Enter your second number: "))
+    print ("The total amout is:")
+    print (num1 + num2)
+    print ("")
