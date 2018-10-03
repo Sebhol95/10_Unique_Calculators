8a7d7c9d0351c277d1faab87e7ccc1bd88056a44
Sebastian Holterhuset <39740798+Sebhol95@users.noreply.github.com>
2018-10-03 21:13:33 +0200

    Add files via upload

---
+++ Linux/Hexadecimal_Calculator.py
@@ -0,0 +1,15 @@
+# Created by Sebhol95 for repository "Calculators" https://github.com/Sebhol95/Calculators
+
+x = 1
+print ("\n")
+print ("The Simple addition Calculator")
+print ("")
+x = int(input("Press 1 to calculate or 0 to exit "))
+while (x > 0):
+    hex = input ("Enter any number in Hexadecimal form: ")
+    if hex == 'x':
+        exit()
+    else:
+        dec = int(hex, 16);
+        print (hex, "in Decimal = ", str(dec));
+        
